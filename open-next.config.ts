f0df69116ea3b51453fadc44ecdfdf8b1a865158
danixxx13 <danielfjesus08@gmail.com>
2026-03-26 14:26:03 -0300

    Adicionando a config do OpenNext

---
+++ open-next.config.ts
@@ -0,0 +1,23 @@
+import type { OpenNextConfig } from 'open-next/types/open-next'
+import cache from '@opennextjs/cloudflare/kvCache'
+
+const config: OpenNextConfig = {
+  default: {
+    override: {
+      wrapper: 'cloudflare-node',
+      converter: 'edge',
+      incrementalCache: async () => cache,
+      tagCache: 'dummy',
+      queue: 'dummy',
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: 'cloudflare-edge',
+      converter: 'edge',
+      proxyExternalRequest: 'fetch',
+    },
+  },
+}
+export default config
